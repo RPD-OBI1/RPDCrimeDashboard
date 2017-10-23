01e1433b79feb802f98e1e53eab7ed0bbcb0ff7e
Martin <AM4337@COR.LOCAL>
2017-10-23 12:32:06 -0400

    All on Z drive; removed ggplotlys; cleaned up section overview

---
+++ CrimeDashboardRepo/knitAll.R
@@ -0,0 +1,67 @@
+
+start <- Sys.time()
+
+## Data prep
+
+source("Z:/Projects/dashboard/RPDCrimeDashboard/dashboardDataPrepWeekly.R")
+
+## Citywide dashboards
+
+rmarkdown::render("citywidedashboard.RMD")
+
+rmarkdown::render("citywideCFSdashboard.RMD")
+
+## Section crime dashboards
+
+rmarkdown::render("SectionCrimeDashboard.RMD", 
+                  params = list(sectionname = "Lake"),
+                  output_file = "LakeCrimeDashboard.html")
+
+rmarkdown::render("SectionCrimeDashboard.RMD", 
+                  params = list(sectionname = "Genesee"),
+                  output_file = "GeneseeCrimeDashboard.html")
+
+rmarkdown::render("SectionCrimeDashboard.RMD", 
+                  params = list(sectionname = "Goodman"),
+                  output_file = "GoodmanCrimeDashboard.html")
+
+rmarkdown::render("SectionCrimeDashboard.RMD", 
+                  params = list(sectionname = "Clinton"),
+                  output_file = "ClintonCrimeDashboard.html")
+
+rmarkdown::render("SectionCrimeDashboard.RMD", 
+                  params = list(sectionname = "Central"),
+                  output_file = "CentralCrimeDashboard.html")
+
+## Section CFS dashboards
+
+rmarkdown::render("SectionCFSDashboard.RMD", 
+                  params = list(sectionname = "Lake"),
+                  output_file = "LakeCFSDashboard.html")
+
+rmarkdown::render("SectionCFSDashboard.RMD", 
+                  params = list(sectionname = "Genesee"),
+                  output_file = "GeneseeCFSDashboard.html")
+
+rmarkdown::render("SectionCFSDashboard.RMD", 
+                  params = list(sectionname = "Goodman"),
+                  output_file = "GoodmanCFSDashboard.html")
+
+rmarkdown::render("SectionCFSDashboard.RMD", 
+                  params = list(sectionname = "Clinton"),
+                  output_file = "ClintonCFSDashboard.html")
+
+rmarkdown::render("SectionCFSDashboard.RMD", 
+                  params = list(sectionname = "Central"),
+                  output_file = "CentralCFSDashboard.html")
+
+## Patrol section overview
+
+rmarkdown::render("PatrolSectionOverview.RMD")
+
+end <- Sys.time()
+took <- difftime(end, start)
+
+fileConn <- file(paste0("HowLongItTook/", Sys.Date(), "took.txt"))
+writeLines(paste(Sys.Date(), took, " ", units(took)), fileConn)
+close(fileConn)
