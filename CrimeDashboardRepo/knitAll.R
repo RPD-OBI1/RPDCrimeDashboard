5c35f6261ed39dfb75431556d1e417b0e49fce43
Martin <AM4337@COR.LOCAL>
2017-10-23 14:57:28 -0400

    CrimesSection - fix Beats tables, to no longer show beats from other sections.
    knitAll - update links. No longer track how long the script takes.

--- CrimeDashboardRepo/knitAll.R
+++ CrimeDashboardRepo/knitAll.R
@@ -3,65 +3,76 @@ start <- Sys.time()
 
 ## Data prep
 
-source("Z:/Projects/dashboard/RPDCrimeDashboard/dashboardDataPrepWeekly.R")
+source("Z:/Projects/dashboard/RPDCrimeDashboard/CrimeDashboardRepo/CrimeDashboardWeeklyDataPrep.R")
 
 ## Citywide dashboards
 
-rmarkdown::render("citywidedashboard.RMD")
+rmarkdown::render("CallsForServiceCitywide.RMD",
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/")
 
-rmarkdown::render("citywideCFSdashboard.RMD")
+rmarkdown::render("CrimesCitywide.RMD",
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/")
 
 ## Section crime dashboards
 
-rmarkdown::render("SectionCrimeDashboard.RMD", 
+rmarkdown::render("CrimesSection.RMD", 
                   params = list(sectionname = "Lake"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "LakeCrimeDashboard.html")
 
-rmarkdown::render("SectionCrimeDashboard.RMD", 
+rmarkdown::render("CrimesSection.RMD", 
                   params = list(sectionname = "Genesee"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "GeneseeCrimeDashboard.html")
 
-rmarkdown::render("SectionCrimeDashboard.RMD", 
+rmarkdown::render("CrimesSection.RMD", 
                   params = list(sectionname = "Goodman"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "GoodmanCrimeDashboard.html")
 
-rmarkdown::render("SectionCrimeDashboard.RMD", 
+rmarkdown::render("CrimesSection.RMD", 
                   params = list(sectionname = "Clinton"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "ClintonCrimeDashboard.html")
 
-rmarkdown::render("SectionCrimeDashboard.RMD", 
+rmarkdown::render("CrimesSection.RMD", 
                   params = list(sectionname = "Central"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "CentralCrimeDashboard.html")
 
 ## Section CFS dashboards
 
-rmarkdown::render("SectionCFSDashboard.RMD", 
+rmarkdown::render("CallsForServiceSection.RMD", 
                   params = list(sectionname = "Lake"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "LakeCFSDashboard.html")
 
-rmarkdown::render("SectionCFSDashboard.RMD", 
+rmarkdown::render("CallsForServiceSection.RMD", 
                   params = list(sectionname = "Genesee"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "GeneseeCFSDashboard.html")
 
-rmarkdown::render("SectionCFSDashboard.RMD", 
+rmarkdown::render("CallsForServiceSection.RMD", 
                   params = list(sectionname = "Goodman"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "GoodmanCFSDashboard.html")
 
-rmarkdown::render("SectionCFSDashboard.RMD", 
+rmarkdown::render("CallsForServiceSection.RMD", 
                   params = list(sectionname = "Clinton"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "ClintonCFSDashboard.html")
 
-rmarkdown::render("SectionCFSDashboard.RMD", 
+rmarkdown::render("CallsForServiceSection.RMD", 
                   params = list(sectionname = "Central"),
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/",
                   output_file = "CentralCFSDashboard.html")
 
 ## Patrol section overview
 
-rmarkdown::render("PatrolSectionOverview.RMD")
+rmarkdown::render("SectionOverlook.RMD",
+                  output_dir = "Z:/Projects/dashboard/RPDCrimeDashboard/")
 
 end <- Sys.time()
 took <- difftime(end, start)
 
-fileConn <- file(paste0("HowLongItTook/", Sys.Date(), "took.txt"))
-writeLines(paste(Sys.Date(), took, " ", units(took)), fileConn)
-close(fileConn)
+took
